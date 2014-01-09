2efa838e85b3ca9c02a4972b16868168d3c384e3
eswann <eric@ericswann.org>
2014-01-08 18:27:00 -0600

    Update to publish

--- deploy/Publish.Burrows.bat
+++ deploy/Publish.Burrows.bat
@@ -1,5 +1,9 @@
 @echo off
 
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {.\archive.ps1; exit $error.Count}"
+
+
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "& {.\publish.ps1 -PackageName 'Burrows'; exit $error.Count}"
 
 
