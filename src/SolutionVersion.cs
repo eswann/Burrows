6e9889957fcdcb4d7da654b8f8c22f0458dc09db
eswann <eric@ericswann.org>
2014-01-28 13:28:13 -0600

    Updated version

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Burrows")]
 [assembly: AssemblyCopyright("Copyright 2014 Eric Swann, et. al. - All rights reserved.")]
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyFileVersion("0.2.4")]
 
 [assembly: AssemblyInformationalVersion("0.2.4.1")]
 [assembly: ComVisible(false)]
