bb2f97785a3b70f6cf66366b185c32229a890887
eswann <eric@ericswann.org>
2014-02-07 17:34:46 -0600

    Added in a purgequeue utility

---
+++ src/Burrows/RabbitUtils/QueuePurge.cs
@@ -0,0 +1,21 @@
+﻿using Burrows.Endpoints;
+using RabbitMQ.Client;
+
+namespace Burrows.RabbitUtils
+{
+    public class QueuePurge
+    {
+        public static void PurgeQueue(string uri)
+        {
+            RabbitEndpointAddress address = RabbitEndpointAddress.Parse(uri);
+
+            using (var connection = address.ConnectionFactory.CreateConnection())
+            {
+                using (IModel model = connection.CreateModel())
+                {
+                    model.QueuePurge(address.Name);
+                }
+            }
+        }
+    }
+}
--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.2.5")]
 [assembly: AssemblyFileVersion("0.2.5")]
 
-[assembly: AssemblyInformationalVersion("0.2.5.1")]
+[assembly: AssemblyInformationalVersion("0.2.5.2")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
