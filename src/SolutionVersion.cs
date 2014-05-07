de08b26bebde7ae7ab1507f314186a38c4bbfd2b
Eric Swann <eric@ericswann.org>
2014-05-07 09:25:55 -0500

    Updated the version because of breaking changes

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Burrows")]
 [assembly: AssemblyCopyright("Copyright 2014 Eric Swann, et. al. - All rights reserved.")]
-[assembly: AssemblyVersion("0.2.5")]
-[assembly: AssemblyFileVersion("0.2.5")]
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
 
-[assembly: AssemblyInformationalVersion("0.2.5.3")]
+[assembly: AssemblyInformationalVersion("0.3.1.0")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 
